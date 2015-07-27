b1137992387ef02e28d8d771f5d42dddcd4f544e
namkha87 <namkha87@gmail.com>
2015-07-27 12:36:37 +0700

    prettifier

--- login_callback/callback.js
+++ login_callback/callback.js
@@ -2,14 +2,15 @@
     'use strict';
 
     var heartbeat_access_token = htbt.util.parse_qs().access_token,
-    	state = htbt.util.parse_qs().state;
+        state = htbt.util.parse_qs().state;
 
     if (heartbeat_access_token) {
-		document.cookie = 'heartbeat_access_token=' + heartbeat_access_token;
-		document.getElementById('login_info').style.display = 'block';
-	}
+        document.cookie = 'heartbeat_access_token=' + heartbeat_access_token;
+        document.getElementById('login_info').style.display = 'block';
+    }
 
-	if (state) {
-		location.href = state;
-	}
+    if (state) {
+        location.href = state;
+    }
 })(window.htbt = window.htbt || {});
+
