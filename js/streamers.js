ef2b7a553917224d571ebcef9e94721c5024b274
ronaldpabilonia <ron@any.tv>
2015-09-02 12:20:33 +0800

    252 heartbeat streamers tasks

--- js/streamers.js
+++ js/streamers.js
@@ -17,7 +17,11 @@
         },
         render: function() {
             var streamers = this.state.streamers,
+                curr_platforms = ['Twitch', 'Hitbox', 'Dailymotion', 'YouTube Gaming'],
                 comp_games = [],
+                curr_position = 0,
+                curr_count = 0,
+                ctr = 0,
                 id,
                 nwc = function (x) {
                     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -58,66 +62,128 @@
                         setTimeout(compile_games, 500);
                     }
                 },
-                filter_cards = function (e) {
-                    id = e.target.id;
-
-                    $.each($('#inner_streamers')[0].children[0].children, function (key, value) {
-                        value.style.display = 'block';
-                        if (!~value.id.indexOf(id) && key) {
-                            value.style.display = 'none';
-                        }
-
-                        if (id === 'all') {
-                            value.style.display = 'block';
-                        }
-                    });
-
-                    $.each($('#inner_streamers')[0].children[2].children, function (key, value) {
-                        value.style.display = 'block';
-                        if (!~value.id.indexOf(id) && key) {
-                            value.style.display = 'none';
-                        }
-
-                        if (id === 'all') {
-                            value.style.display = 'block';
-                        }
-                    });
-
-                    $.each($('button'), function (key, value) {
-                        value.disabled = false;
-
-                        if (value.id === id) {
-                            value.disabled = true;
-                        }
-                    });
+                filter_platform = function () {
+                    ctr++;
+                    if ($('#c_twitch')[0] && ctr === 2) {
+                        $('.filter_platform').click(function () {
+                            !~curr_platforms.indexOf(this.value) ? curr_platforms.push(this.value) : curr_platforms.splice(curr_platforms.indexOf(this.value), 1);
+                            $.each($('.platform_icon'), function (key, value) {
+                                var parent = value.parentNode.parentNode.parentNode.parentNode;
+                                parent.style.display = ~curr_platforms.indexOf(value.alt) ? 'block' : 'none';
+                            });
+                        });
+                    }
+                    else {
+                        setTimeout(filter_platform, 500)
+                    }
+                },
+                process_platform = function (url) {
+                    var img;
+                    if (typeof url === 'undefined' || ~url.indexOf('twitch')) {
+                        return <img className='platform_icon' alt='Twitch' src='images/twicon.png'></img>;
+                    }
+                    if (~url.indexOf('hitbox')) {
+                        img = <img className='platform_icon' alt='Hitbox' src='images/hbicon.png'></img>;
+                    }
+                    if (~url.indexOf('dailymotion')) {
+                        img = <img className='platform_icon' alt='Dailymotion' src='images/dmicon.png'></img>;
+                    }
+                    if (~url.indexOf('youtube')) {
+                        img = <img className='platform_icon' alt='YouTube Gaming' src='images/gyicon.png'></img>;
+                    }
+                    return img;
+                },
+                process_stream_preview = function (platform, preview, status) {
+                    var bg;
+                    if (preview) {
+                        bg = <img className='lazy' src={preview} alt={status} title={status}/>
+                    }
+                    if (!preview && !platform) {
+                        bg = <img className='lazy' src='images/noprevtw.jpg' alt={status} title={status}/>
+                    }
+                    if (!preview && platform === 'Hitbox') {
+                        bg = <img className='lazy' src='images/noprevhb.jpg' alt={status} title={status}/>
+                    }
+                    if (!preview && platform === 'Dailymotion') {
+                        bg = <img className='lazy' src='images/noprevdm.jpg' alt={status} title={status}/>
+                    }
+                    if (!preview && platform === 'YouTube Gaming') {
+                        bg = <img className='lazy' src='images/noprevgy.jpg' alt={status} title={status}/>
+                    }
+                    return bg;
+                },
+                hide_all = function () {
+                    if ($('.online_streamer_column')[0]) {
+                        $.each($('.online_streamer_column')[0].parentNode.children, function (key, a) {
+                            if (key && key <= 9) {
+                                a.style.display = 'block';
+                            }
+                        });
+                    }
+                    else {
+                        setTimeout(hide_all, 500);
+                    }
+                },
+                show_next_content = function (pos, count) {
+                    if (pos / 200 % 1 === 0) {
+                        $.each($('.online_streamer_column')[0].parentNode.children, function (key, a) {
+                            if (key && key <= (9 + count)) {
+                                a.style.display = 'block';
+                            }
+                        });
+                        $.each($('.offline_streamer_column')[0].parentNode.children, function (key, a) {
+                            if (key && key <= (9 + count)) {
+                                a.style.display = 'block';
+                            }
+                        });
+                    }
                 };
 
+            filter_platform();
             compile_games();
+            hide_all();
 
             if (typeof streamers.online === 'undefined') {
                 return null;
             }
+
             $('#loader_img').hide();
             $('#game_filter_dropdown').attr('style', 'display: block');
+
             $('img.lazy').lazyload({
                 event: 'scrollStop',
                 threshold : 200
             });
+
+            $(window).scroll(function () {
+                if ($(window)[0].scrollY / 200 % 1 === 0) {
+                    curr_position += 200;
+                    curr_count += 15;
+                    show_next_content(curr_position, curr_count);
+                }
+            });
+
             return (<div id='inner_streamers'>
                 <div className='row'>
                     <div className='filter'>
-                        <button id='all' className='waves-effect waves-light waves-light white black-text btn' onClick={filter_cards}>All</button>
-                        <button id='twitch' className='waves-effect waves-light waves-light deep-purple lighten-2 btn' onClick={filter_cards}>Twitch</button>
-                        <button id='hitbox' className='waves-effect waves-light waves-light light-green btn' onClick={filter_cards}>Hitbox</button>
-                        <button id='dailymotion' className='waves-effect waves-light waves-light blue lighten-2 btn' onClick={filter_cards}>Dailymotion</button>
+                        <p>
+                            <input className='filter_platform' value='YouTube Gaming' type="checkbox" id="c_youtube_gaming" defaultChecked/>
+                            <label htmlFor="c_youtube_gaming">YouTube Gaming</label>
+                            <input className='filter_platform' value='Twitch' type="checkbox" id="c_twitch" defaultChecked/>
+                            <label htmlFor="c_twitch">Twitch</label>
+                            <input className='filter_platform' value='Hitbox' type="checkbox" id="c_hitbox" defaultChecked/>
+                            <label htmlFor="c_hitbox">Hitbox</label>
+                            <input className='filter_platform' value='Dailymotion' type="checkbox" id="c_dailymotion" defaultChecked/>
+                            <label htmlFor="c_dailymotion">Dailymotion</label>
+                        </p>
                     </div>
                     { streamers.online.map(function (onstreamer) {
-                        return (<div id={onstreamer.url} className='col s12 m6 l4'>
+                        return (<div id={onstreamer.url} className='col s12 m6 l4 online_streamer_column' style={{display: 'none'}}>
                                 <div className='online_streamer'>
                                     <a href={onstreamer.url} target='_blank'>
                                         <div className='card' alt={onstreamer.status} title={onstreamer.status}>
                                             <div className='card-image'> 
-                                                <img className='lazy' src={onstreamer.stream_preview ? onstreamer.stream_preview : 'images/noprev.jpg'} alt={onstreamer.status} title={onstreamer.status}/>
+                                                {process_stream_preview(onstreamer.platform, onstreamer.stream_preview, onstreamer.status)}
                                             </div> 
                                             <img className='lazy' className='user_thumb' src={onstreamer.profile_img ? onstreamer.profile_img : 'images/ukuser.png'} alt={onstreamer.dname} title={onstreamer.dname}/> 
                                             <div className='user_info'>
@@ -136,6 +202,8 @@
                                                         {onstreamer.lang ? <small><span className='lang'>{onstreamer.lang}</span></small> : ''}
                                                         &nbsp;
                                                         {onstreamer.mature ? <small><span className='mature'>M</span></small> : ''}
+                                                        &nbsp;
+                                                        {process_platform(onstreamer.url)}
                                                     </div>
                                                 </div>
                                             </div>
@@ -149,12 +217,12 @@
                 <hr/>
                 <div className='row'>
                     {streamers.offline.map(function(offstreamer) {
-                        return offstreamer.dname ? (<div id={offstreamer.url} className='col s12 m6 l4'>
+                        return offstreamer.dname ? (<div id={offstreamer.url} className='col s12 m6 l4 offline_streamer_column' style={{display: 'none'}}>
                             <div className='offline_streamer'>
                                 <a href={offstreamer.url} target='_blank'>
                                     <div className='card' alt={offstreamer.status} title={offstreamer.status}>
                                         <div className='card-image'> 
-                                            <img className='lazy' src='images/noprev.jpg' alt={offstreamer.status} title={offstreamer.status}/>
+                                            {process_stream_preview(offstreamer.platform, offstreamer.stream_preview, offstreamer.status)}
                                         </div> 
                                         <img className='lazy' className='user_thumb' src={offstreamer.profile_img ? offstreamer.profile_img : 'images/ukuser.png'} alt={offstreamer.dname} title={offstreamer.dname}/> 
                                             <div className='user_info'>
@@ -172,7 +240,9 @@
                                                         &nbsp;
                                                         {offstreamer.lang ? <small><span className='lang'>{offstreamer.lang}</span></small> : ''}
                                                         &nbsp;
-                                                        {offstreamer.mature ? <small><span className='mature'>M</span></small> : ''}
+                                                        {offstreamer.platform}
+                                                        &nbsp;
+                                                        {process_platform(offstreamer.url)}
                                                     </div>
                                                 </div>
                                             </div>
